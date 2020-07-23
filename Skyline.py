ce4d115e8bc77c829d80b66067f1355a29ea42d7
Cosmo05 <67003752+Cosmo05@users.noreply.github.com>
2020-07-23 20:17:54 +0530

    Update Skyline.py
    Corrected the input

--- Skyline.py
+++ Skyline.py
@@ -2,12 +2,13 @@ import numpy as np
 from math import *
 
 N=int(input('Enter the no. of bulidings'))
-w= [int(w) for w in input("Enter all the building's width value: ").split()]
-l=[int(l) for l in input("Enter all the building's length value: ").split()]
+w= [int(w) for w in input("Enter all the building's HEIGHT value: ").split(',')]
+l=[int(l) for l in input("Enter all the building's WIDTH value: ").split(',')]
 
 
 total_length=sum(l)
 height=min(w)
 Area_max=total_length*height
 print(Area_max)
+
 	
