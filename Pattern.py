8fe5c778a5a1fa87c30daf3c1d11cfed9613dd99
geekninja1708 <68696719+geekninja1708@users.noreply.github.com>
2020-08-07 01:28:42 -0400

    Update Pattern.py

--- Pattern.py
+++ Pattern.py
@@ -1,13 +1,15 @@
 #print a pattern 1->23->345-->4567
 N=int(input('Enter the no. of rows'))
 i=1
+k=1
 while i<=N:
 	j=1
 	while j<=i:
-		print(j,end="")
-		j=j+1
+		print(k,end="")
+		j+=1
+		k+=1
 	print()
-	i=i+1
+	i+=1
 
 	
 	
